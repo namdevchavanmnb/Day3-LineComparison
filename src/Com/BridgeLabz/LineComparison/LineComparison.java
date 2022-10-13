98045ba3a1c18a7469d9ef44c55876db8a5be7d8
namdevchavanmnb@gmail.com <namdevchavanmnb@gmail.com>
2022-10-13 22:56:47 +0530

    Line Comparison

---
+++ src/Com/BridgeLabz/LineComparison/LineComparison.java
@@ -0,0 +1,10 @@
+package Com.BridgeLabz.LineComparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Line Comparision Program");
+
+	}
+
+}
